7a42e12c64ac12f19ec329f5368796198fc30f29
Shota Iguchi <shota-iguchi@cookpad.com>
2019-03-17 18:09:09 +0900

    Refactoring test codes
    ref https://github.com/golang/go/wiki/TableDrivenTests

--- doublearray_test.go
+++ doublearray_test.go
@@ -5,11 +5,46 @@ import (
 	"testing"
 )
 
-var (
-	keys        = []string{"Lorem", "ipsum", "dolor", "sit", "amet", "consectetur", "adipiscing", "elit", "sed", "do", "eiusmod", "tempor", "incididunt", "ut", "labore", "et", "dolore", "magna", "aliqua", "Ut", "enim", "ad", "minim", "veniam", "quis", "nostrud", "exercitation", "ullamco", "laboris", "nisi", "ut", "aliquip", "ex", "ea", "commodo", "consequat", "Duis", "aute", "irure", "dolor", "in", "reprehenderit", "in", "voluptate", "velit", "esse", "cillum", "dolore", "eu", "fugiat", "nulla", "pariatur", "Excepteur", "sint", "occaecat", "cupidatat", "non", "proident", "sunt", "in", "culpa", "qui", "officia", "deserunt", "mollit", "anim", "id", "est", "laborum"}
-	validKeys   = []string{"Lorem", "ipsum", "dolor", "sit", "amet", "consectetur", "adipiscing", "elit", "sed", "do", "eiusmod", "tempor", "incididunt", "ut", "labore", "et", "dolore", "magna", "aliqua"}
-	invalidKeys = []string{"Lore", "ipsu", "olor", "it", "met", "conctetur", "adiping"}
-)
+var keys = []string{"Lorem", "ipsum", "dolor", "sit", "amet", "consectetur", "adipiscing", "elit", "sed", "do", "eiusmod", "tempor", "incididunt", "ut", "labore", "et", "dolore", "magna", "aliqua", "Ut", "enim", "ad", "minim", "veniam", "quis", "nostrud", "exercitation", "ullamco", "laboris", "nisi", "ut", "aliquip", "ex", "ea", "commodo", "consequat", "Duis", "aute", "irure", "dolor", "in", "reprehenderit", "in", "voluptate", "velit", "esse", "cillum", "dolore", "eu", "fugiat", "nulla", "pariatur", "Excepteur", "sint", "occaecat", "cupidatat", "non", "proident", "sunt", "in", "culpa", "qui", "officia", "deserunt", "mollit", "anim", "id", "est", "laborum"}
+
+var exactMatchSearchTests = []struct {
+	in  string
+	out bool
+}{
+	{"Lorem", true},
+	{"ipsum", true},
+	{"dolor", true},
+	{"sit", true},
+	{"amet", true},
+	{"consectetur", true},
+	{"adipiscing", true},
+	{"elit", true},
+	{"sed", true},
+	{"do", true},
+	{"eiusmod", true},
+	{"tempor", true},
+	{"incididunt", true},
+	{"ut", true},
+	{"labore", true},
+	{"et", true},
+	{"dolore", true},
+	{"magna", true},
+	{"aliqua", true},
+	{"Lore", false},
+	{"lorem", false},
+	{"ipsu", false},
+	{"olor", false},
+	{"i", false},
+}
+
+var containsMatchTests = []struct {
+	in  string
+	out bool
+}{
+	{"lorem ipsum is simply dummy text", true},
+	{"Lorem is simply dummy", true},
+	{"lorem ipsu is simply dummy txt", false},
+}
 
 var d DoubleArray
 
@@ -18,33 +53,27 @@ func init() {
 }
 
 func TestExactMatchSearch(t *testing.T) {
-	ok := true
-	for _, key := range validKeys {
-		ok = ok && d.ExactMatchSearch(key)
-	}
+	for _, test := range exactMatchSearchTests {
+		t.Run(test.in, func(t *testing.T) {
+			result := d.ExactMatchSearch(test.in)
 
-	for _, key := range invalidKeys {
-		ok = ok && !d.ExactMatchSearch(key)
-	}
+			if result != test.out {
+				t.Errorf("got %t, want %t", result, test.out)
+			}
+		})
 
-	if !ok {
-		t.Fail()
 	}
 }
 
 func TestContainsMatch(t *testing.T) {
-	var ok bool
+	for _, test := range containsMatchTests {
+		t.Run(test.in, func(t *testing.T) {
+			result := d.ContainsMatch(test.in)
 
-	text := "lorem ipsum is simply dummy text"
-	ok = d.ContainsMatch(text)
-
-	invalidTexts := []string{"lorem is simply dummy txt", "lorem ipsu is simply dummy txt"}
-	for _, text := range invalidTexts {
-		ok = ok && !d.ContainsMatch(text)
-	}
-
-	if !ok {
-		t.Fail()
+			if result != test.out {
+				t.Errorf("got %t, want %t", result, test.out)
+			}
+		})
 	}
 }
 
